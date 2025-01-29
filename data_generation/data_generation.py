dc50278bc0f63f744406fae6a7041af48a47b944
Dingyi <15351412+DingyiLai@user.noreply.gitee.com>
2025-01-29 22:00:33 +0800

    change SNR

--- data_generation/data_generation.py
+++ data_generation/data_generation.py
@@ -11,6 +11,8 @@ from tensorflow.keras.layers import Dense, Flatten, ReLU
 from time import time
 from scipy.stats import poisson, gamma, norm
 import multiprocessing as mp
+from concurrent.futures import ThreadPoolExecutor
+
 # from sddr import Sddr  # Assuming you have pyssdr installed and configured correctly
 import logging
 from datetime import datetime
@@ -33,9 +35,11 @@ def build_dnn(input_dim, layer_sizes=[32, 16], activation="relu"):
         Dense(layer_sizes[1], activation=activation),
         Dense(1, activation=None)
     ])
+    model.compile(optimizer='adam', loss='mse')  # Compile the model
+
     return model
 
-def generate_gp_image(grid_size=28, length_scale=0.2, random_state=None):
+def generate_gp_image(grid_size=28, length_scale=0.2, random_state=None): # , SNR_compare=[2,4]
     np.random.seed(random_state)
     x = np.linspace(0, 1, grid_size)
     y = np.linspace(0, 1, grid_size)
@@ -43,145 +47,193 @@ def generate_gp_image(grid_size=28, length_scale=0.2, random_state=None):
     coords = np.vstack([X.ravel(), Y.ravel()]).T
 
     kernel = RBF(length_scale=length_scale)
-    gp = GaussianProcessRegressor(kernel=kernel)
-    intensity = gp.sample_y(coords, n_samples=1).reshape(grid_size, grid_size)
-    return scale_to_range(intensity)
+    gp = GaussianProcessRegressor(kernel=kernel, random_state=random_state)
+    intensity = gp.sample_y(coords, n_samples=1, random_state=random_state).reshape(grid_size, grid_size)
+    
+    intensity_scaled = scale_to_range(intensity)
+    
+    return intensity_scaled
 
 # ---------------------------
 # Simulation Functions
 # ---------------------------
 
-def generate_linear_effects(n_samples, K, I, alpha, random_state=None):
+def generate_linear_effects(n_samples, I, K, alpha, random_state=None): # SNR_compare, 
     np.random.seed(random_state)
-    X = np.random.normal(size=(n_samples, I))
-    X_scaled = scale_to_range(X, 0, 1)
-    linear_effects = np.zeros((n_samples, K))
+    X = np.random.normal(size=(I, n_samples, K)) # The same basis for all SNRs
+    X_scaled = np.zeros_like(X)
+    linear_effects1 = np.zeros((I, n_samples, K))
+    
+    for i in range(I):
+        X_scaled[i] = scale_to_range(X[i], 0, 1)
+        for k in range(K):
+            linear_effects1[i, :, k] = alpha[k][i] * X_scaled[i][:, k]
+        
+    return X_scaled, scale_to_range(linear_effects1)
 
-    for k in range(1, K + 1):
-        for i in range(I):
-            linear_effects[:, k - 1] += alpha[k][i] * X_scaled[:, i]
-    return X_scaled, scale_to_range(linear_effects)
-
-def generate_nonlinear_effects(n_samples, K, J, beta, random_state=None):
+def generate_nonlinear_effects(n_samples, J, K, beta, random_state=None): #SNR_compare, 
     np.random.seed(random_state)
-    Z = np.random.uniform(0, 1, size=(n_samples, J))
-    nonlinear_effects = np.zeros((n_samples, K))
+    Z = np.random.uniform(0, 1, size=(J, n_samples, K)) # doesn't need to scale
+    nonlinear_effects1 = np.zeros((J, n_samples, K))
 
-    for k in range(1, K + 1):
-        for j in range(J):
-            nonlinear_effects[:, k - 1] += beta[k][j](Z[:, j])
-    return Z, scale_to_range(nonlinear_effects)
+    for j in range(J):
+        for k in range(K):
+            nonlinear_effects1[j, :, k] = beta[k][j](Z[j][:, k])
+        
+    return Z, scale_to_range(nonlinear_effects1) #a_Z1, a_Z2, , scale_to_range(nonlinear_effects2)
 
 def generate_unstructured_effects(images, dnn_model, K):
     n_samples = len(images)
     unstructured_effects = np.zeros((n_samples, K))
-
-    for k in range(K):
-        unstructured_output = np.random.rand(n_samples)  # Mock prediction
-        unstructured_effects[:, k] = scale_to_range(unstructured_output)
-    return unstructured_effects
-
-def combine_effects(unstructured_effects, linear_effects, nonlinear_effects, SNR):
-    K = linear_effects.shape[1]
-    N = linear_effects.shape[0]  # Number of data points
-    etas = np.zeros((N, K))
-    a = np.zeros((N, K))  # Intercepts for each data point and K
-
-    for k in range(K):
-        etas[:, k] = linear_effects[:, k] + nonlinear_effects[:, k] + unstructured_effects[:, k]
-        range_etas = np.ptp(etas[:, k])
-        a[:, k] = np.random.normal(0, range_etas/SNR, size=N)  # Gaussian noise for each data point
-        etas[:, k] += a[:, k]  # Add noise to each data point
     
-    return etas, a
+    # Predict unstructured effects using the DNN model
+    predictions = dnn_model.predict(images)
 
-def simulate_response(etas, distribution="poisson", K=2):
+    # Extract the penultimate layer's output
+    penultimate_layer_model = Model(inputs=dnn_model.input, outputs=dnn_model.layers[-2].output)
+    penultimate_output = penultimate_layer_model.predict(images)
+
+    # Extract the weights and bias from the final layer
+    final_layer_weights, final_layer_bias = dnn_model.layers[-1].get_weights()
+
+    for k in range(K):
+        unstructured_effects[:, k] = scale_to_range(predictions[:, 0])
+
+    return unstructured_effects, penultimate_output, final_layer_weights, final_layer_bias # \hat{U}_k, \hat{\psi}_k, \hat{b}_k
+
+def combine_effects(scenario, rep, save_path, unstructured_effects, linear_effects, nonlinear_effects, distribution="poisson", SNR_list=[1,8]):
+    for s in SNR_list:
+        add_SNR(scenario, rep, save_path, unstructured_effects, linear_effects, nonlinear_effects, distribution, s)
+
+def add_SNR(scenario_index, rep, save_path, unstructured_effects, linear_effects, nonlinear_effects, distribution="poisson", SNR=1):
+    os.makedirs(save_path, exist_ok=True)
+    scenario_index += f"_SNR{SNR}_rep{rep}"
+    K = linear_effects.shape[2]
+    N = linear_effects.shape[1]  # Number of data points
+    etas = np.zeros((N, K))
+    k = 0
+    etas[:, k] = linear_effects[:, :, k].sum(axis=0) + nonlinear_effects[:, :, k].sum(axis=0) + unstructured_effects[:, k]
+    range_etas = np.ptp(etas[:, k])
+    std_eta = np.std(etas[:, k])
+    if distribution == "poisson" or distribution == "gamma":
+        a = std_eta * SNR - range_etas
+        etas[:, k] += a
+    elif distribution == "gaussian":
+        for k in range(K):
+            etas[:, k] = linear_effects[:, :, k].sum(axis=0) + nonlinear_effects[:, :, k].sum(axis=0) + unstructured_effects[:, k]
+        a = np.exp(etas[:, 1]) * SNR - np.ptp(etas[:, 0]) # a2 in (17)
+        etas[:, 1] += a
+    else:
+        raise ValueError(f"Unsupported distribution: {distribution}")
+    
+    
+    save_with_var_name(a, 'a', 'npy', save_path, scenario_index, rep)
+    save_with_var_name(etas, 'etas', 'npy', save_path, scenario_index, rep)
+    # Generate response based on distribution
+    responses = simulate_response(etas, distribution)
+    save_with_var_name(responses, 'responses', 'npy', save_path, scenario_index, rep)
+
+def simulate_response(etas, distribution="poisson"):
     n_samples = etas.shape[0]
-
-    if distribution == "poisson" and K == 1:
+    if distribution == "poisson":
         mu = np.exp(etas[:, 0])
         return np.random.poisson(mu)
-    elif distribution == "gamma" and K == 2:
+    elif distribution == "gamma":
         mu = np.exp(etas[:, 0])
         sigma = 1.0
         shape = (mu / sigma) ** 2
         scale = sigma ** 2 / mu
-        return np.random.gamma(shape, scale, n_samples)
-    elif distribution == "gaussian" and K == 2:
+        return np.random.gamma(shape, scale, n_samples) # scale must be non-negative
+    elif distribution == "gaussian":
         mu = etas[:, 0]
         sigma = np.exp(etas[:, 1])
         return np.random.normal(mu, sigma, n_samples)
     else:
         raise ValueError(f"Unsupported distribution: {distribution}")
 
-def save_results(scenario, rep, n_samples, X, Z, images, etas, responses, unstructured_effects, intercepts, save_path):
-    # save_path = os.path.join(os.environ["TMPDIR"], "output")
-    os.makedirs(save_path, exist_ok=True)
-    scenario_index = '_'.join(map(str, scenario))
+def save_with_var_name(var, var_name, var_type, save_path, scenario_index, rep):
+    if var_type == 'npy':
+        np.save(f"{save_path}/{var_name}_{scenario_index}.npy", var)
+    if var_type == 'keras':
+        var.save(f"{save_path}/{var_name}_{scenario_index}.keras")
+    logging.info(f"Saved {var_name} to {save_path}/{var_name}_{scenario_index}.npy")
     
-    # Save data as before
-    np.save(f"{save_path}/X_{scenario_index}_rep{rep}.npy", X)
-    np.save(f"{save_path}/Z_{scenario_index}_rep{rep}.npy", Z)
-    np.save(f"{save_path}/images_{scenario_index}_rep{rep}.npy", images)
-    np.save(f"{save_path}/etas_{scenario_index}_rep{rep}.npy", etas)
-    np.save(f"{save_path}/responses_{scenario_index}_rep{rep}.npy", responses)
-
-    # Save intercepts and unstructured effects separately
-    np.save(f"{save_path}/intercepts_{scenario_index}_rep{rep}.npy", intercepts)
-    np.save(f"{save_path}/unstructured_effects_{scenario_index}_rep{rep}.npy", unstructured_effects)
-
-    logging.info(f"Saved dataset {scenario_index}_rep{rep}, responses, intercepts, and unstructured_effects.")
-
 # ---------------------------
 # Generate Task Function for Parallel Execution
 # ---------------------------
 
-
-def generate_task(task_index, scenario_index, rep, scenarios, alpha, beta, grid_size, save_path):
-    scenario = scenarios[scenario_index]
-    n_samples, distribution, K, SNR = scenario
+def generate_task(n_sample, distribution_list, SNR_list, grid_size, alpha_l, beta_nl, n_rep, 
+                  save_path, compute_type='parallel'):
     
+    # Set random seed for reproducibility
+    np.random.seed(n_sample+n_rep)
     # Build the DNN model (mock)
     dnn_model = build_dnn(grid_size * grid_size)
 
-    logging.info(f"Generating dataset: Scenario {scenario_index} | Replication {rep} | {scenario}")
-    
+    logging.info(f"Generating dataset: Number of obs {n_sample} | Replication {n_rep}")
+    I = 2
+    K = 2 # predefine
     # Generate structured and unstructured effects
-    X, linear_effects = generate_linear_effects(n_samples, K, 2, alpha, random_state=task_index)
-    Z, nonlinear_effects = generate_nonlinear_effects(n_samples, K, 2, beta, random_state=task_index)
-    images = np.array([generate_gp_image(grid_size, random_state=task_index * 5000 + i) for i in range(n_samples)])
-    unstructured_effects = generate_unstructured_effects(images, dnn_model, K)
-    logging.info(f"Generated X, Z, images and unstructured_effects: Scenario {scenario_index} | Replication {rep} | {scenario}")
-    
-    # Combine effects and calculate intercepts
-    etas, intercepts = combine_effects(unstructured_effects, linear_effects, nonlinear_effects, SNR)
-    logging.info(f"Generated etas and intercepts: Scenario {scenario_index} | Replication {rep} | {scenario}")
-    
-    # Generate response based on distribution
-    responses = simulate_response(etas, distribution, K)
-    logging.info(f"Generated responses: Scenario {scenario_index} | Replication {rep} | {scenario}")
-    
-    # Save datasets and responses, including intercepts and unstructured effects
-    save_results(scenario, rep, n_samples, X, Z, images, etas, responses, unstructured_effects, intercepts, save_path)
+    X, linear_effects = generate_linear_effects(n_sample, I, K, alpha_l, random_state=n_sample+n_rep)
+    Z, nonlinear_effects = generate_nonlinear_effects(n_sample, I, K, beta_nl, random_state=n_sample+n_rep)
+    images = np.zeros((n_sample, grid_size, grid_size))
 
+    for i in range(n_sample):
+        images[i] = generate_gp_image(grid_size, length_scale=0.2, random_state=(n_sample+n_rep)*n_sample-i)
+    
+    # Flatten images before passing to the DNN model
+    flattened_images = images.reshape(n_sample, -1)  
+    unstructured_effects, U_k, psi_k, b_k = generate_unstructured_effects(flattened_images, dnn_model, K)
+
+    logging.info(f"Generated X, Z, images and unstructured_effects: Number of obs {n_sample} | Replication {n_rep}")
+    scenario_index = '_'.join(map(str, ['n', n_sample, 'rep',n_rep]))
+
+    save_with_var_name(X, 'X', 'npy', save_path, scenario_index, n_rep)
+    save_with_var_name(linear_effects, 'linear_effects', 'npy', save_path, scenario_index, n_rep)
+    save_with_var_name(Z, 'Z', 'npy', save_path, scenario_index, n_rep)
+    save_with_var_name(nonlinear_effects, 'nonlinear_effects', 'npy', save_path, scenario_index, n_rep)
+    save_with_var_name(images, 'images', 'npy', save_path, scenario_index, n_rep)
+    save_with_var_name(unstructured_effects, 'unstructured_effects', 'npy', save_path, scenario_index, n_rep)
+    save_with_var_name(U_k, 'U_k', 'npy', save_path, scenario_index, n_rep)
+    save_with_var_name(psi_k, 'psi_k', 'npy', save_path, scenario_index, n_rep)
+    save_with_var_name(b_k, 'b_k', 'npy', save_path, scenario_index, n_rep)
+    save_with_var_name(dnn_model, 'dnn_model', 'keras', save_path, scenario_index, n_rep)
+    
+    # Combine effects
+    # Parallel processing
+    if compute_type == 'parallel':
+        # parellel computing can't be nested by default, use concurrent computing for each executer
+        with ThreadPoolExecutor() as inner_executor:
+            inner_executor.map(combine_effects, [(scenario_index, n_rep, save_path, unstructured_effects, linear_effects, nonlinear_effects, d, SNR_list) 
+                                        for d in distribution_list])
+    if compute_type == 'serial':
+        for d in distribution_list:
+            print(d)
+            combine_effects(scenario_index, n_rep, save_path, unstructured_effects, linear_effects, nonlinear_effects, d, SNR_list)    
+    
 # ---------------------------
 # Parallel Execution Function
 # ---------------------------
 
-def parallel_generate(scenarios, grid_size, alpha, beta, n_cores=4, n_rep=4, save_path=None):
-    logging.info("Starting parallel dataset generation...")
+def scenarios_generate(n_list, distribution_list, SNR_list, grid_size, alpha_l, beta_nl, n_rep, n_cores,
+                  compute_type='parallel'):
+    logging.info(f"Starting {compute_type} dataset generation...")
     start_time = datetime.now()
-    
-    # Create (scenario_index, rep) combinations for parallel processing
-    task_list = [(i, r) for i in range(len(scenarios)) for r in range(n_rep)]
-    logging.info(f"number of task_list: {len(task_list)}.")
-    logging.info(f"number of cores used: {n_cores}.")
-    
-    # Use pool.starmap to pass multiple arguments to the method
-    with mp.Pool(n_cores) as pool:
-        pool.starmap(generate_task, [(idx, i, r, scenarios, alpha, beta, grid_size, save_path) 
-                                     for idx, (i, r) in enumerate(task_list)])
-    
+    n_r = [(i, r) for i in n_list for r in range(n_rep)]
+    if compute_type == 'parallel':
+        # Use pool.starmap to pass multiple arguments to the method
+        save_path = os.path.join(os.environ["TMPDIR"], "output")
+        with mp.Pool(n_cores) as pool:
+            pool.starmap(generate_task, [(i, distribution_list, SNR_list, grid_size, 
+                                            alpha_l, beta_nl, r, save_path, compute_type) 
+                                        for (i, r) in n_r])
+    if compute_type == 'serial':
+        save_path = "../data_generation/output_local"
+        for (i, r) in n_r:
+            generate_task(n_sample=i, distribution_list=distribution_list, 
+                        SNR_list=SNR_list, grid_size=grid_size, alpha_l=alpha_l,
+                        beta_nl=beta_nl, n_rep=r, 
+                        save_path=save_path, compute_type=compute_type)
     end_time = datetime.now()
     logging.info(f"Parallel dataset generation completed in {end_time - start_time}.")
 
@@ -189,19 +241,16 @@ def parallel_generate(scenarios, grid_size, alpha, beta, n_cores=4, n_rep=4, sav
 # Scenario Setup
 # ---------------------------
 
-scenarios = [
-    (100, "poisson", 1, 2), (500, "poisson", 1, 2), (1000, "poisson", 1, 2),
-    (100, "poisson", 1, 4), (500, "poisson", 1, 4), (1000, "poisson", 1, 4),
-    (100, "gamma", 2, 2), (500, "gamma", 2, 2), (1000, "gamma", 2, 2), 
-   (100, "gamma", 2, 4), (500, "gamma", 2, 4), (1000, "gamma", 2, 4), 
-   (100, "gaussian", 2, 2), (500, "gaussian", 2, 2), (1000, "gaussian", 2, 2),
-   (100, "gaussian", 2, 4), (500, "gaussian", 2, 4), (1000, "gaussian", 2, 4)
-]
+# n_list = [100, 500, 1000]
+n_list = [10]
+distribution_list = ["poisson", "gamma", "gaussian"]
+SNR_list=[1,8]
 
 grid_size = 28
-
+n_core = mp.Pool(mp.cpu_count())
+print(f"Number of cores: {n_core}")
 # Define coefficients
-alpha = {1: [3, -1], 2: [-0.5, 6]}
+alpha_l = {0: [3, -1], 1: [-0.5, 6]}
 
 # Define named functions for nonlinear effects
 def nonlinear_effect_1_z1(z1):
@@ -217,10 +266,10 @@ def nonlinear_effect_2_z2(z2):
     return scale_to_range(0.1 * np.sqrt(z2))
 
 # Example coefficients for nonlinear effects
-beta = {
-    1: [nonlinear_effect_1_z1, nonlinear_effect_1_z2],
-    2: [nonlinear_effect_2_z1, nonlinear_effect_2_z2]
+beta_nl = {
+    0: [nonlinear_effect_1_z1, nonlinear_effect_1_z2],
+    1: [nonlinear_effect_2_z1, nonlinear_effect_2_z2]
 }
-save_path = os.path.join(os.environ["TMPDIR"], "output")
-parallel_generate(scenarios, grid_size, alpha, beta, n_cores=152, n_rep=100, save_path=save_path)
 
+scenarios_generate(n_list, distribution_list, SNR_list, grid_size, alpha_l, beta_nl, n_rep=1, n_cores=n_core,
+                  compute_type='parallel')
